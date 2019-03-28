5988332d1c81d925cc25ea6744d23be9af5bc283
Nabeelah Ali <naal@microsoft.com>
2019-03-28 16:19:29 +0100

    Add in installation CTA.

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,43 @@ import React, { Component } from "react";
 import Timer from "./components/Timer";
 import "./App.css";
 
-class App extends Component {
+interface State {
+  showInstallCTA: boolean;
+}
+
+class App extends Component<any, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      showInstallCTA: false
+    };
+  }
+
+  componentDidMount() {
+    let deferredPrompt;
+
+    window.addEventListener("beforeinstallprompt", e => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+
+      this.setState({
+        showInstallCTA: true
+      });
+      console.log("Got prompt");
+    });
+  }
+
   render() {
-    return <Timer />;
+    return (
+      <div className="container">
+        {this.state.showInstallCTA && (
+          <div className="install-cta">Add to home screen</div>
+        )}
+        <Timer />
+      </div>
+    );
   }
 }
 
